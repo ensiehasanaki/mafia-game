7ee62c712f7d471096aa2b6f059586b4b84559f4
ensie <nc.hasanaki@gmail.com>
2021-11-20 19:58:59 +0330

    layout

---
+++ src/layout.js
@@ -0,0 +1,12 @@
+import "./layout.css";
+
+export default function Layout() {
+    return (
+        <div className="layout">
+            <nav className="menu">top menu</nav>
+            <section className="section">main page wrapper</section>
+            <side className="side">extra information wrapper</side>
+            <footer className="footer">footer</footer>
+        </div>
+    );
+}
