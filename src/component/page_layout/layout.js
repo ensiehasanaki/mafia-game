32c7bfc316587c6ebbfe820f8a9091953b3b5af9
ensie <nc.hasanaki@gmail.com>
2021-12-11 11:22:16 +0330

    mainWrapper edite

--- src/component/page_layout/layout.js
+++ src/component/page_layout/layout.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./layout.css";
 import Footer from "./footer/footer";
 import Menu from "./menu/menu";
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
     return (
@@ -10,7 +11,9 @@ export default function Layout() {
                 <Menu />
             </nav>
             <div className="middel">
-                <section className="section">main page wrapper</section>
+                <section className="section">
+                    <Outlet />
+                </section>
                 <side className="side">extra information wrapper</side>
             </div>
 
---
+++ src/component/page_layout/menu/menu_component/playerList.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+import data from "./playerList.json";
+
+export default function PlayerList() {
+    return (
+        <div>
+            <div>nc</div>
+        </div>
+    );
+}
