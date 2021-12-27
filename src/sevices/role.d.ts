cf5dd8ec62d18be3fdf19cb510e8c181af35d03e
ensie <nc.hasanaki@gmail.com>
2021-12-27 16:31:20 +0330

    add role

---
+++ src/sevices/role.d.ts
@@ -0,0 +1,5 @@
+export declare interface Role {
+    title: "citizen" | "mafia" | "doctor";
+    definition: string;
+    class: "citizen" | "mafia";
+}
---
+++ src/sevices/role.js
@@ -0,0 +1,17 @@
+export const citizen = {
+    title: "citizen",
+    definition: "klj",
+    class: "citizen",
+};
+
+export const doctor = {
+    title: "doctor",
+    definition: "kgljsdf",
+    class: "citizen",
+};
+
+export const mafia = {
+    title: "mafia",
+    definition: "klj",
+    class: "mafia",
+};
